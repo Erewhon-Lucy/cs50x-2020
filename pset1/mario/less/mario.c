ab93e574d447110b55d7bf2eaeb41ad208bf7b24
Erewhon-Lucy <1663523296@qq.com>
2020-09-04 16:40:40 +0800

    complete mario less

---
+++ pset1/mario/less/mario.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void repeat(char c, int times)
+{
+    while (times > 0)
+    {
+        printf("%c", c);
+        times--;
+    }
+}
+
+int main(void)
+{
+    int height;
+
+    do
+    {
+        height = get_int("Height: ");
+
+    }
+    while (height < 1 || height > 8);
+
+    for (int i = 1; i <= height; i++)
+    {
+        repeat(' ', height - i);
+        repeat('#', i);
+
+        printf("\n");
+    }
+}
